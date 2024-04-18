b9ed0dfaaf9be4a5b856cf1a5cffa6f727ecf3f2
MuhammadUsama-hub <muhammadusama798i@gmai.com>
2024-04-17 23:07:13 -0700

    just created

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let wordsCounter = (para: string): number[] => {
+  para = para.trim(); // removes leadind /trailing white spaces
+  let subString = para.split(" "); // converting string to array
+  let updateSubString = subString.filter((e) => e !== ""); //creates new array with empty strings
+  let charCounts = 0;
+  updateSubString.forEach((e) => (charCounts += e.length)); // counts total paragraph characters
+  return [updateSubString.length, charCounts]; // finally the words counts done!
+};
+//asking data from user
+let answers = await inquirer.prompt([
+  { message: "Write a paragraph : ", type: "string", name: "para" },
+]);
+let results = wordsCounter(answers.para);
+console.log(
+  `Paragraph contains ${results[0]} Words & ${results[1]} Characters.`
+);
